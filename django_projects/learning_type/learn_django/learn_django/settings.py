dd461acc90a5bcae8ce7b4b21e22ae115184d9ff
Deepak Verma <deekgsbv@gmail.com>
2020-07-23 15:18:03 +0530

    added static files

--- django_projects/learning_type/learn_django/learn_django/settings.py
+++ django_projects/learning_type/learn_django/learn_django/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'learn_django.urls'
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR]
